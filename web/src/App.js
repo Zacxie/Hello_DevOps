c1546d50b657caa4ccbd4bc23846c4468d3dfa29
Sebastian Bjerre <sebastian.bjerre@pm.me>
2021-09-29 01:22:53 +0200

    Basic login page - no styling yet

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  import React from 'react';
+  import './App.css';
+  import LoginPage from "./loginPage";
+  import MainLayout from './components/MainLayout'
+  import Navbar from "./components/navbar/Navbar";
+  import {HashRouter, Route, Switch} from "react-router-dom";
+  
+  function App() {
+      return (
+          <div className="App">
+            <header className="App-header">
+                <HashRouter>
+                    <Switch>
+                        <Route path="/login">
+                            <LoginPage/>
+                        </Route>
+                        <Route path="/">
+                        <div className="container">
+                           <MainLayout />
+                        </div>
+                        </Route>
+                    </Switch>
+                </HashRouter>
+            </header>
+        </div>
+    );
 }
 
 export default App;
